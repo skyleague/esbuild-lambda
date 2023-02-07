0b4d7185db97640868d402780a2a0d2c2a8c4840
MickVanDuijn <m.vanduijn@zefiros.io>
2023-02-07 16:25:21 +0100

    fix: correctly format regex file filters (#19)

--- src/plugins/json.ts
+++ src/plugins/json.ts
@@ -5,7 +5,7 @@ import fs from 'fs'
 export const jsonPlugin: Plugin = {
     name: 'json-loader',
     setup: (compiler) => {
-        compiler.onLoad({ filter: /.json$/ }, async (args) => {
+        compiler.onLoad({ filter: /\.json$/ }, async (args) => {
             const content = await fs.promises.readFile(args.path, 'utf-8')
             return {
                 contents: `module.exports = JSON.parse(${JSON.stringify(JSON.stringify(JSON.parse(content)))})`,
