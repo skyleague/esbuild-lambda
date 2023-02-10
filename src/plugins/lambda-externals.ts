b498d6b72699b4b24c89ade3da75ec5a12033aea
Eugen Podaru <eugen.podaru@live.com>
2023-02-10 16:36:21 +0100

    fix: win paths resolution (#20)
    * fix(build): update esbuild plugins
    
    * fix win path resolution
    
    ---------
    
    Co-authored-by: Eugen Podaru <eugen.podaru@knab.nl>

--- src/plugins/lambda-externals.ts
+++ src/plugins/lambda-externals.ts
@@ -61,10 +61,10 @@ export function lambdaExternalsPlugin({
                 // Finally, it it's NEITHER a relative import NOR a node built-in libary, determine the relevant version for the Lambda handler
                 try {
                     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-                    externals[packageName] = require(`${root}/node_modules/${packageName}/package.json`).version
+                    externals[packageName] = require(path.join(root, 'node_modules', packageName, 'package.json')).version
                 } catch (err) {
                     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-                    externals[packageName] = require(`${packageName}/package.json`).version
+                    externals[packageName] = require(path.join(packageName, 'package.json')).version
                 }
                 return { path: args.path, external: true }
             })
